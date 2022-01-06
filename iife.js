a6196c5be5ca5c7d1b35637c114c41b2e8f1147c
Davit Martirosyan <phpencoding@gmail.com>
2022-01-06 16:44:23 +0400

    d

--- iife.js
+++ iife.js
@@ -1,32 +1,32 @@
-// const  post = [
-//     {title: "Post One", body: "first post"},
-//     {title: "Post Two", body: "second post"},
-// ];
-// (getPost = function(){
-//     setTimeout(() => {
-//         let output = "";
-//         post.forEach((p, index) => {
-//             output +=  `<li>${p.title} -> ${p.body}</li>`
-//         })
-//         document.body.innerHTML = output;
-//     }, 1000)
-// })();
+const  post = [
+    {title: "Post One", body: "first post"},
+    {title: "Post Two", body: "second post"},
+];
+(getPost = function(){
+    setTimeout(() => {
+        let output = "";
+        post.forEach((p, index) => {
+            output +=  `<li>${p.title} -> ${p.body}</li>`
+        })
+        document.body.innerHTML = output;
+    }, 1000)
+})();
 
-// function createPost(p){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(()=> {
-//             post.push(p);
-//             const err = false;
-//             if(!err)
-//                 resolve();
-//             reject();
-//         });
-//     })
-// }
+function createPost(p){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            post.push(p);
+            const err = false;
+            if(!err)
+                resolve();
+            reject();
+        });
+    })
+}
 
-// createPost({title: "Post Three", body:"Third Post"})
-//     .then(getPost)
-//     .catch(err => err);
+createPost({title: "Post Three", body:"Third Post"})
+    .then(getPost)
+    .catch(err => err);
 
 
 const promise1 = Promise.resolve("Hello World");
